1b480b58be61f7ed77a6e99515d9bb47503b026f
Bilal Ahmed <ahmedalibilal26@gmail.com>
2020-07-24 12:41:03 +0530

    initial mesage

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./App.css";
+import ClasaCounter from "./Components/ClasaCounter";
+import HookCounter from "./Components/HookCounter";
+import HookCounter2 from "./Components/HookCounter2";
+import HookCounterThree from "./Components/HookCounterThree";
+import HooKCounterFour from "./Components/HooKCounterFour";
+import HookCounterEffectOne from "./Components/HookCounterEffectOne";
+import HooksMouse from "./Components/HooksMouse";
+import IntervalClassCounter from "./Components/IntervalClassCounter";
+import IntervalHookCounter from "./Components/IntervalHookCounter";
+import DataFechting from "./Components/DataFechting";
+import Hero from "./Components/Hero";
+import ErrorBoundary from "./Components/ErrorBoundary";
+import ClickCounter from "./Components/ClickCounter";
+import HoverCounter from "./Components/HoverCounter";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ClasaCounter /> */}
+      {/* <HookCounter /> */}
+      {/* <HookCounter2 /> */}
+      {/* <HookCounterThree /> */}
+      {/* <HooKCounterFour /> */}
+      {/* <HookCounterEffectOne /> */}
+      {/* <HooksMouse /> */}
+      {/* <IntervalClassCounter />
+      <IntervalHookCounter /> */}
+      {/* <DataFechting /> */}
+      {/* <ErrorBoundary>
+        <Hero heroName="Batman" />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Superman" />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="joker" />
+      </ErrorBoundary> */}
+      {/* <ClickCounter name="Bilal" /> */}
+      {/* <HoverCounter /> */}
+    </div>
+  );
+}
+
+export default App;
